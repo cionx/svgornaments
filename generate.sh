3ec1f05b4ca8a2d170f65d203dff9a104fd9792c
Jendrik Stelzner <jendrikstelzner@web.de>
2021-04-08 09:54:17 +0200

    Rewrote the build script.

--- generate.sh
+++ generate.sh
@@ -1,13 +1,30 @@
 #!/bin/env sh
 
-for i in {1..196}
+# PREPARATION
+echo "" > generate.log
+TOTAL=10
+
+# MAIN LOOP
+for ((i = 1; i <= TOTAL; i++))
 do
 	number=$(printf "%03d" $i)
+	echo -en "\r Converting $i/$TOTAL"
+
+	echo "$number" >> generate.log
+
 	sed "s/number/$i/g" template.tex > vectorian$number.tex
-	latexmk vectorian$number.tex
-	inkscape vectorian$number.pdf --export-area-drawing --export-filename svg/vectorian$number.svg
+	xelatex vectorian$number.tex \
+		>> generate.log 2>> generate.log
+
+	inkscape vectorian$number.pdf \
+		--export-area-drawing \
+		--export-filename svg/vectorian$number.svg \
+		>> generate.log 2>> generate.log
+
+	echo "\n\n" >> generate.log
 done
 
-latexmk -c vectorian*.tex
-latexmk -C vectorian*.tex
+# REMOVE AUXILIARY FILES
+latexmk -c vectorian*.tex >> generate.log 2>> generate.log
+latexmk -C vectorian*.tex >> generate.log 2>> generate.log
 rm vectorian*.tex
