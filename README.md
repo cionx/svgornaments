221d92320052c20425911febabd18421691192e7
Jendrik Stelzner <jendrikstelzner@web.de>
2021-04-08 08:43:43 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository contains svg versions of the “vectorian ornaments” from the [`pgfornament` package][1].
+
+[1]: https://ctan.org/pkg/pgfornament
+
+
---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+
+for i in {1..196}
+do
+	number=$(printf "%03d" $i)
+	sed "s/number/$i/g" template.tex > vectorian$number.tex
+	latexmk vectorian$number.tex
+	inkscape vectorian$number.pdf --export-filename svg/vectorian$number.svg
+done
+
+latexmk -c vectorian*.tex
+latexmk -C vectorian*.tex
+rm vectorian*.tex
